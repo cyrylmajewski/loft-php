f9ec196cc11fdcd0fb7ba5271e1546667c3639a1
Your Name <your.name@example.com>
2021-06-21 21:53:15 +0200

    table homework

--- homework_6/homework.php
+++ homework_6/homework.php
@@ -12,10 +12,17 @@
 <body>
     <table>
       <?php
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 1; $i < 11; $i++) {
             echo "<tr>";
             for ($j = 1; $j < 11; $j++) {
-                echo "<td>" . ($j * ($i + 1)) . "</td>";
+                $number = $j * $i;
+                if ($i % 2 === 0 && $j % 2 === 0) {
+                    echo "<td> ({$number}) </td>";
+                } else if (($i - 2) % 2 === 1 && ($j - 2) % 2 === 1) {
+					echo "<td>[{$number}]</td>";
+                } else {
+                    echo "<td> {$number} </td>";
+                }
             }
             echo "</tr>";
         }
